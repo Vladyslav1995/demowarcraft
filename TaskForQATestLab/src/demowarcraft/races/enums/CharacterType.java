654e82b1341e7fa6c39fec2653a51de1da99f97e
vlad <vlad@vladyslav.(none)>
2017-07-21 03:10:43 +0300

    first commit

--- TaskForQATestLab/src/demowarcraft/races/enums/CharacterType.java
+++
@@ -1,6 +0,0 @@
-
-package demowarcraft.races.enums;
-
-public enum CharacterType {
-    MAGICIAN, BOWMAN, WARRIOR, CROSSBOWMAN, SHAMAN, GOBLIN, NECROMANT, HUNTER, ZOMBIE
-}
--- TaskForQATestLab/src/demowarcraft/races/enums/Race.java
+++
@@ -1,5 +0,0 @@
-package demowarcraft.races.enums;
-
-public enum Race {
-    UNDEAD, ORCS, HUMAN, ELVES
-}
